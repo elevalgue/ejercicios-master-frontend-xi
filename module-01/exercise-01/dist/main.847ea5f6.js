5bdde0155efa657f101be407a0bda4541f96de0d
elena <elena.valenzuela.guerrero@gmail.com>
2021-10-20 17:06:08 +0200

    Nest loops into base color styles

--- module-01/exercise-01/dist/main.847ea5f6.js
+++ module-01/exercise-01/dist/main.847ea5f6.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58038" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49914" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
