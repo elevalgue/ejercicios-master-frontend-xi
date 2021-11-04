7e5aa4835ca1166e704a352d06d1fbe5c2a5f5aa
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-04 17:35:51 +0100

    Rename folders

--- module-02-language/01-javascript-fundamentals/14-values/src/js/main.js
+++ module-02-language/01-javascript-fundamentals/14-values/src/js/main.js
@@ -1,2 +1,9 @@
 console.log("************** PRACTICE *********************");
 console.log("14 Values");
+
+function values(obj) {
+  // Implementation here
+}
+
+// Ejemplo:
+console.log(values({ id: 31, duration: 310, name: "long video", format: "mp4" })); // [31, 310, "long video", "mp4"]
