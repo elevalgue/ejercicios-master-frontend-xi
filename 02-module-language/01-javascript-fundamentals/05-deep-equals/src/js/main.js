a2e199ce2f2f256c1fbdd15e888ebbfab4053066
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-08 13:12:01 +0100

    Add exercise 05 folder

---
+++ 02-module-language/01-javascript-fundamentals/05-deep-equals/src/js/main.js
@@ -0,0 +1,29 @@
+console.log("************** PRACTICE *********************");
+console.log("05 - Deep Equals");
+
+console.log("----- Apartado A ------");
+
+const original = {
+  name: "Evan",
+  surname: "Smith",
+  country: "USA",
+};
+
+function clone(obj) {
+  return { ...obj };
+}
+
+console.log(clone(original));
+console.log("-----------------------");
+
+console.log("----- Apartado B ------");
+
+var a = { name: 'Maria', surname: 'Ibañez', country: 'SPA' };
+var b = { name: 'Luisa', age: 31, married: true };
+
+function merge(source, target) {
+  return {...target, ...source}
+}
+
+console.log(merge(a, b));
+console.log("-----------------------");
