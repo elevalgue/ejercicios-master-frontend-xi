aa85ff4bdd59f95674bcaa4c862ae47f84716c45
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-11 11:18:42 +0100

    Solve deepEqual

--- 02-module-languages/01-javascript-fundamentals/05-deep-equals/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/05-deep-equals/src/js/main.js
@@ -6,26 +6,56 @@ console.log("----- Apartado A ------");
 var user = { name: "María", age: 30 };
 var clonedUser = { name: "María", age: 30 };
 
-isEqual = (a, b) => {
-  // 1. We're setting the keys of each one of the objects
-  const aKeys = Object.keys(user);
-  const bKeys = Object.keys(clonedUser);
+// isEqual = (a, b) => {
+  
+//   // 1. We're setting the keys of each one of the objects
+//   const objAkeys = Object.keys(user);
+//   const objBkeys = Object.keys(clonedUser);
 
-  // 2. A easy thing to test if they both are equals is to check that they have the same number of items.
-  if (aKeys.length !== bKeys.length) {
-    return false;
+//   // 2. A easy thing to test if they both are equals is to check that they have the same number of items.
+//   if (objAkeys.length !== objBkeys.length) {
+//     console.log(objAkeys.length );
+//     return false;
+//   }
+
+//   // 3. We're going through the objects keys
+//   for (let objKey of objAkeys) {
+//     // we're grabbing the keys of the objs
+//     if (a[objKey] !== b[objKey]) {
+//       return false; 
+//     }
+//   }
+// }
+
+function isEqual(a, b) {
+  const objAkeys = Object.keys(user);
+  const objBkeys = Object.keys(clonedUser);
+
+if (a === b) {
+    return true;
   }
+else if ((typeof a == "object" && a != null) && (typeof b == "object" && b != null)) {
+  
+    if (objAkeys.length != objBkeys.length)
+      return false;
 
-  // 3. We're going through the objects keys
-  for (let aKey of aKeys) {
-    // we're grabbing the keys of the objs
-    if (a[aKey] !== b[bKey]) {
-      return false; 
+    for (let prop in a) {
+      if (b.hasOwnProperty(prop))
+      {  
+        if (!isEqual(a[prop], b[prop]))
+          return false;
+      }
+      else
+        return false;
     }
+    return true;
   }
+  else 
+    return false;
 }
 
 
-a === b;
+
+
 console.log(isEqual(user, clonedUser));
 
