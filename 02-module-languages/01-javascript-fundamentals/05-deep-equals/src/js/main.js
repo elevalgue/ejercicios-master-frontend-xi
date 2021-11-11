93939cb6f8501a02385d01c7c63681ec77ea471a
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-11 11:22:07 +0100

    Clean :broom:

--- 02-module-languages/01-javascript-fundamentals/05-deep-equals/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/05-deep-equals/src/js/main.js
@@ -6,28 +6,8 @@ console.log("----- Apartado A ------");
 var user = { name: "María", age: 30 };
 var clonedUser = { name: "María", age: 30 };
 
-// isEqual = (a, b) => {
-  
-//   // 1. We're setting the keys of each one of the objects
-//   const objAkeys = Object.keys(user);
-//   const objBkeys = Object.keys(clonedUser);
-
-//   // 2. A easy thing to test if they both are equals is to check that they have the same number of items.
-//   if (objAkeys.length !== objBkeys.length) {
-//     console.log(objAkeys.length );
-//     return false;
-//   }
-
-//   // 3. We're going through the objects keys
-//   for (let objKey of objAkeys) {
-//     // we're grabbing the keys of the objs
-//     if (a[objKey] !== b[objKey]) {
-//       return false; 
-//     }
-//   }
-// }
-
 function isEqual(a, b) {
+  // 1. We're setting the keys of each one of the objects
   const objAkeys = Object.keys(user);
   const objBkeys = Object.keys(clonedUser);
 
@@ -35,10 +15,10 @@ if (a === b) {
     return true;
   }
 else if ((typeof a == "object" && a != null) && (typeof b == "object" && b != null)) {
-  
+    // 2. A easy thing to test if they both are equals is to check that they have the same number 
     if (objAkeys.length != objBkeys.length)
       return false;
-
+    // 3. We're going through the objects keys
     for (let objKey in a) {
       if (b.hasOwnProperty(objKey))
       {  
@@ -53,9 +33,6 @@ else if ((typeof a == "object" && a != null) && (typeof b == "object" && b != nu
   else 
     return false;
 }
-
-
-
 
 console.log(isEqual(user, clonedUser));
 
