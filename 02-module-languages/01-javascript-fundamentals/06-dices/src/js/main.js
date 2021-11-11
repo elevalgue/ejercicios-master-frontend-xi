be81b4691c29cf9c4f56d3228614c75b56ce9e18
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-11 11:31:31 +0100

    Add exercises 06 & 07 folders

---
+++ 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
@@ -0,0 +1,4 @@
+console.log("************** PRACTICE *********************");
+console.log("06 - Dices");
+
+console.log("----- Apartado A ------");
---
+++ 02-module-languages/01-javascript-fundamentals/07-hoisting/src/js/main.js
@@ -0,0 +1,4 @@
+console.log("************** PRACTICE *********************");
+console.log("07 - Hoisting");
+
+console.log("----- Apartado A ------");
