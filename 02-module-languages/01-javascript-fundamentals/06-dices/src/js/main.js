a78f1b236ba2fd9c9c86d8b60ff8bdef3782c16c
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-11 12:24:25 +0100

    Get random number

--- 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
@@ -1,3 +1,11 @@
 console.log("************** PRACTICE *********************");
 console.log("06 - Dices");
 
+let diceA;
+let diceB;
+
+function getNumbers() {
+  return Math.floor(Math.random() * (6 - 1)) + 1; 
+}
+
+console.log(getNumbers());
