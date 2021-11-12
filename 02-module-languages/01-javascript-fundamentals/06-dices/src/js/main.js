6c4e36d454e5ddf8e4202fd4e1c96bdc7dedf0ea
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-12 18:46:41 +0100

    Add folders

--- 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
@@ -1,11 +1,2 @@
 console.log("************** PRACTICE *********************");
-console.log("06 - Dices");
-
-let diceA;
-let diceB;
-
-function getNumbers() {
-  return Math.floor(Math.random() * (6 - 1)) + 1; 
-}
-
-console.log(getNumbers());
+console.log("09 - Primes");
