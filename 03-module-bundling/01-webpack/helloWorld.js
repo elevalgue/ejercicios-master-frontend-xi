5af095c9ec35340f3a9be5c201167db38f2b8835
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-13 18:45:02 +0100

    Add basic to webpack.config.js to use babel

---
+++ 03-module-bundling/01-webpack/helloWorld.js
@@ -0,0 +1,5 @@
+const greeting = "Hey,"; 
+const firstName = "Maricarmen!";
+const messageToDisplay = ` ${greeting} ${firstName}`;
+
+document.write(messageToDisplay); 
---
+++ 03-module-bundling/01-webpack/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: ["./helloWorld.js"],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+}
