d013ffa960bd6626dac18d1f65fd36ea9ed4e688
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-16 18:17:28 +0100

    Add small changes

--- 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
@@ -1,33 +1,33 @@
 console.log("************** PRACTICE *********************");
-console.log("09 - Primes");
-
-// let diceA; 
-// let diceB; 
+console.log("06 - Dices");
 
 function renderDices() {
   let diceA; 
   let diceB;
-  console.log(diceA, diceB, 'hello world from render');
+  console.log(diceA, diceB);
   
   function rollDices(min, max) {
     diceA = Math.random(min, max) * (6 - 1) + 1;
     diceB = Math.random(min, max) * (6 - 1) + 1;
+    console.log('Cruza los dedos y tira los dados 🎲');
   }
 
-  function reset() {
-    diceA = null; 
-    diceB = null;
-    console.log(diceA, diceB, 'hello world from reset');
-  }
+  // function reset() {
+  //   diceA = null; 
+  //   diceB = null;
+  //   console.log(diceA, diceB, 'los dados deberían ser null');
+  // }
   
   return {
     renderMessage: function () {
       rollDices()
-      console.log(diceA, diceB, 'hello, its me');
-      reset();
+      if (diceA === 6|| diceB === 6) {
+        console.log('🎉🎉 Enhorabuena! tenemos ganador🎉🎉 🏆');
+      }
     }
   }
 }
+// reset();
 
 let roll = renderDices();
 roll.renderMessage();
