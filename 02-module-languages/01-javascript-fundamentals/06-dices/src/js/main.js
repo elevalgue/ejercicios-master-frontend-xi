dc796d8d47755e28cf5578cd031248e8a8c83c72
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-16 17:40:12 +0100

    Reset dices

--- 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
@@ -1,2 +1,20 @@
 console.log("************** PRACTICE *********************");
 console.log("09 - Primes");
+
+// let diceA; 
+// let diceB; 
+
+function renderDices() {
+  let diceA; 
+  let diceB;
+  console.log(diceA, diceB, 'hello world from render');
+  
+  function reset() {
+    diceA = null; 
+    diceB = null;
+    console.log(diceA, diceB, 'hello world from reset');
+  }
+  reset();
+}
+
+renderDices();
