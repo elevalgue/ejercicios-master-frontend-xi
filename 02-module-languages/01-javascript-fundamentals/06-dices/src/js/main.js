5c51aeac7b5e8795db37cc381d0400785e463d71
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-17 13:47:08 +0100

    Reorder code

--- 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
@@ -1,41 +1,44 @@
 console.log("************** PRACTICE *********************");
 console.log("06 - Dices");
 
-function renderDices() {
-  let diceA; 
+function initializeDices() {
+  let diceA;
   let diceB;
 
-  function reset() {
-    diceA = null; 
-    diceB = null;
-    console.log(diceA, diceB, 'los dados deberían ser null');
-  }
-  
-  function rollDices(max, min) {
-    diceA = Math.floor(Math.random(max, min) * (6 - 1) + 1);
-    diceB = Math.floor(Math.random(max, min) * (6 - 1) + 1);
-    console.log('Cruza los dedos y tira los dados 🎲');
-    console.log(diceA);
-    // reset();
-  }
-  console.log(diceA, 'hello');
-  
-  
-  return {
-    renderMessage: function () {
-      rollDices()
-      if (diceA === 6 && diceB === 6) {
-        console.log('🎉🎉 Enhorabuena! tenemos ganador🎉🎉 🏆');
-      } else {
-        console.log(`El primer dado es ${diceA} y el segndo ${diceB}`);
+  const renderDices = {
+    reset() {
+      let diceA = null;
+      let diceB = null;
+      console.log(diceA, diceB, 'los dados deberían ser null');
+    },
+    
+    // function reset() {
+    //   diceA = null; 
+    //   diceB = null;
+    //   console.log(diceA, diceB, 'los dados deberían ser null');
+    // }
+    
+    rollDices(max, min) {
+      diceA = Math.floor(Math.random(max, min) * (6 - 1) + 1);
+      diceB = Math.floor(Math.random(max, min) * (6 - 1) + 1);
+      console.log('Cruza los dedos y tira los dados 🎲');
+      console.log(diceA);
+      return {
+        renderMessage: function () {
+          // rollDices()
+          if (diceA === 6 && diceB === 6) {
+            console.log('🎉🎉 Enhorabuena! tenemos ganador🎉🎉 🏆');
+          } else {
+            console.log(`El primer dado es ${diceA} y el segndo ${diceB}`);
+          }
+        }
       }
     }
   }
-
 }
-
-let roll = renderDices();
-roll.renderMessage();
-roll.renderMessage();
-roll.renderMessage();
-roll.renderMessage();
+  
+//   let roll = rollDices();
+//   roll.renderMessage();
+// roll.renderMessage();
+// roll.renderMessage();
+// roll.renderMessage();
