da36ecc56aee0ff290cc0adb3dd9ab8976e37fc3
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-17 17:43:28 +0100

    Invoke functions

--- 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/06-dices/src/js/main.js
@@ -16,15 +16,14 @@ function initializeDices() {
       diceA = Math.floor(Math.random(max, min) * (6 - 1) + 1);
       diceB = Math.floor(Math.random(max, min) * (6 - 1) + 1);
       console.log('Cruza los dedos y tira los dados 🎲');
-      console.log(diceA);
       return {
         renderMessage: function () {
-          // rollDices()
           if (diceA === 6 && diceB === 6) {
             console.log('🎉🎉 Enhorabuena! tenemos ganador🎉🎉 🏆');
           } else {
             console.log(`El primer dado es ${diceA} y el segndo ${diceB}`);
           }
+          console.log(diceA);
         }
       }
     }
@@ -32,9 +31,8 @@ function initializeDices() {
 
   return renderDices; 
 }
-  
-//   let roll = rollDices();
-//   roll.renderMessage();
+
+let roll = initializeDices();
+roll.rollDices();
 // roll.renderMessage();
-// roll.renderMessage();
-// roll.renderMessage();
+roll.reset();
