a5d42c5973e016352ebd7deb94fc86e86875f379
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-23 11:13:15 +0100

    Link styles

--- 02-module-languages/01-javascript-fundamentals/07-hoisting/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/07-hoisting/src/js/main.js
@@ -1,6 +1,48 @@
 console.log("************** PRACTICE *********************");
 console.log("07 - Hoisting");
-console.log("----- Apartado A ------");
 
+console.log("----- Apartado A ------");
+function f() {
+  console.log(a);
+  console.log(g());
+
+  var a = "good job!";
+  function g() {
+    return a;
+  }
+  console.log(a);
+}
+
+f();
+console.log("-----------------------");
+
+console.log("----- Apartado B ------");
+var a = 1;
+
+(function() {
+  console.log(a);
+  var a = 2;
+  b = 4;
+  var c = 3;
+})();
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log("-----------------------");
+
+console.log("----- Apartado B ------");
+f();
+var a = 1;
+
+function f() {
+  console.log(a);
+  b = 4;
+  var c = 3;
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
 console.log("-----------------------");
 
