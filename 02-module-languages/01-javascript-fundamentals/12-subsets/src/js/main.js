6e2c7c8a1df3f98463b281a76b8b6d5368e7b7aa
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-23 19:21:04 +0100

    Add Subsets exercise :sparkles:

--- 02-module-languages/01-javascript-fundamentals/12-subsets/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/12-subsets/src/js/main.js
@@ -1,2 +1,20 @@
 console.log("************** PRACTICE *********************");
 console.log("12 - Subsets");
+
+function subsets(word) {
+  const reversedWord = []; 
+  for (let index = 1; index < word.length; index++) {
+    reversedWord.push(word.slice(index, word.length)); 
+  }
+
+  return reversedWord; 
+}
+
+
+// function subsets(word) {
+//   return word.split("")
+//     .map(character => character)
+//     .join(""); 
+// }
+
+console.log(subsets('message')); // ["essage", "ssage", "sage", "age", "ge", "e"]
