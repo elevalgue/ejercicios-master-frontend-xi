2ebece78759870abd24c636a8ea9743fd3c6d889
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-23 14:01:54 +0100

    Add primes exercise :sparkles:

--- 02-module-languages/01-javascript-fundamentals/09-primes/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/09-primes/src/js/main.js
@@ -1,34 +1,19 @@
-console.log('14 - Values');
 console.log('************** PRACTICE *********************');
-console.log('01 - Escribe una función que devuelva una lista de valores de todas las propiedades de un objeto:');
-function values({...obj}) {
-    return Object.values(obj)
+console.log('09 - Primes');
+
+const isPrime = number => {
+    for (let i = 2; i < number; i++)
+        if (number % i === 0) return false;
+    return number > 1;
 }
 
-// Ejemplo:
-console.log(values({ id: 31, duration: 310, name: 'long video', format: 'mp4' })); // [31, 310, 'long video', 'mp4']
-
-console.log('************** PRACTICE *********************')
-console.log('02: Challenge - Evita añadir las propiedades heredadas en caso de ser instancia de una clase:');
-
-// Ejemplo:
-const objPerson = {
-    name: 'Silvio Fernández',
-    job: 'musician',
-};
-
-function values2(objPerson) {
-    console.log(Object.values(objPerson))
-};
-
-function Person(name, job) {
-    this.name = name;
-    this.job = job;
+function showPrimes(from, to) {
+    for (let number = from; number <= to; number++) {
+        isPrime(number)
+        ? console.log(`El número ${number} es primo`)
+        : console.log(`El número ${number} no es primo`);
+    }
 }
 
-Person.prototype.work = function() {
-    console.log("I'm a musician, I'm an artist");
-};
-
-const john = new Person('Johny', 'singer');
-values2(john); // ['John', 'singer')]; en vez de ['John'; function() { console.log("I'm a musician, I'm an artist"); }]
+console.log(isPrime());
+console.log(showPrimes(1, 100));
