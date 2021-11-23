276ddd48ade1aba09173ada907dcd685eeaf5cc9
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-23 12:02:16 +0100

    Solve includes exercise

--- 02-module-languages/01-javascript-fundamentals/08-includes/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/08-includes/src/js/main.js
@@ -1,34 +1,16 @@
-console.log('14 - Values');
 console.log('************** PRACTICE *********************');
-console.log('01 - Escribe una función que devuelva una lista de valores de todas las propiedades de un objeto:');
-function values({...obj}) {
-    return Object.values(obj)
+console.log('08 - Includes');
+
+function includes(array, value) {
+  for (const item of array) {
+    if (item === value) {
+      return true;
+      
+    } else {
+      return false; 
+    }
+  }
 }
 
-// Ejemplo:
-console.log(values({ id: 31, duration: 310, name: 'long video', format: 'mp4' })); // [31, 310, 'long video', 'mp4']
-
-console.log('************** PRACTICE *********************')
-console.log('02: Challenge - Evita añadir las propiedades heredadas en caso de ser instancia de una clase:');
-
-// Ejemplo:
-const objPerson = {
-    name: 'Silvio Fernández',
-    job: 'musician',
-};
-
-function values2(objPerson) {
-    console.log(Object.values(objPerson))
-};
-
-function Person(name, job) {
-    this.name = name;
-    this.job = job;
-}
-
-Person.prototype.work = function() {
-    console.log("I'm a musician, I'm an artist");
-};
-
-const john = new Person('Johny', 'singer');
-values2(john); // ['John', 'singer')]; en vez de ['John'; function() { console.log("I'm a musician, I'm an artist"); }]
+console.log(includes([1, 2, 3], 3)); // true
+console.log(includes([1, 2, 3], 0)); // false
