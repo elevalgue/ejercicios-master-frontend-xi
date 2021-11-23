0b5c1afe1950583208c657dddb5098bc147126e0
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-23 19:23:47 +0100

    Change routes

--- 02-module-languages/01-javascript-fundamentals/12-subsets/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/12-subsets/src/js/main.js
@@ -2,19 +2,17 @@ console.log("************** PRACTICE *********************");
 console.log("12 - Subsets");
 
 function subsets(word) {
-  const reversedWord = []; 
+  const reversedWord = [];
   for (let index = 1; index < word.length; index++) {
     reversedWord.push(word.slice(index, word.length)); 
   }
-
   return reversedWord; 
 }
 
-
-// function subsets(word) {
-//   return word.split("")
-//     .map(character => character)
-//     .join(""); 
-// }
+function subsets2(word) {
+  const iteratedWord = word.split("").slice(1);
+  return iteratedWord.map((character, index) => iteratedWord.join("").slice(index));
+}
 
 console.log(subsets('message')); // ["essage", "ssage", "sage", "age", "ge", "e"]
+console.log(subsets2('message'), 'hola');
