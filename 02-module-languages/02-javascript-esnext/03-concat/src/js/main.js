d226cee61fea4b10651ee3005820cd7318c5846d
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-25 21:30:10 +0100

    Add concat exercise

--- 02-module-languages/02-javascript-esnext/03-concat/src/js/main.js
+++ 02-module-languages/02-javascript-esnext/03-concat/src/js/main.js
@@ -1,2 +1,19 @@
 console.log('************** PRACTICE *********************');
 console.log('03 - Concat');
+
+console.log("----- Apartado A ------");
+const numbers = [1, 2, 3, 4, 5]; 
+const letters = ["a", "b", "c", "d", "e"]; 
+const boolean = [true, false];
+const arrayN = ["jan", "feb", "mar", "apr", "may"];
+
+const concat = (a, b) => [...a, ...b];
+
+console.log(concat(numbers, letters));
+console.log("-----------------------");
+
+console.log("----- Apartado B ------");
+const concatMulti = (array1, array2, array3, arrayN) => [...array1, ...array2, ...array3, ...arrayN];
+
+console.log(concatMulti(numbers, letters, boolean, arrayN));
+console.log("-----------------------");
