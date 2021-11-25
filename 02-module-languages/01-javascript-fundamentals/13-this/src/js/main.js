6dda669bb7af30b324c3d3d2afadeaa789c3ee2e
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-25 17:39:10 +0100

    Add exercis3

--- 02-module-languages/01-javascript-fundamentals/13-this/src/js/main.js
+++ 02-module-languages/01-javascript-fundamentals/13-this/src/js/main.js
@@ -1,2 +1,20 @@
 console.log("************** PRACTICE *********************");
 console.log("13 - This");
+
+var surname = 'Pérez';
+var person = {
+  name: 'Juan',
+  surname: 'González',
+  wife: {
+    name: 'Ana',
+    surname: 'Jiménez',
+    getSurname: function () {
+      return this.surname;
+    },
+  },
+};
+
+console.log(person.wife.getSurname());
+var surnameFunction = person.wife.getSurname;
+console.log(surnameFunction());
+console.log(surnameFunction.call(person));
