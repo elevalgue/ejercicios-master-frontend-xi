fd5fdeb15d88dcc44ec730ae604afcafcad62251
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-29 10:13:19 +0100

    Add new folder

---
+++ 03-bundling/prueba/webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ["./src/students.js"],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      scriptLoading: "blocking",
+    }),
+  ],
+};
