f43bde3e4cfccdeb16287cbb1a69a8f430573a24
elena <elena.valenzuela.guerrero@gmail.com>
2021-11-29 11:25:45 +0100

    Add css loaders & config styles

--- 03-bundling/prueba/webpack.config.js
+++ 03-bundling/prueba/webpack.config.js
@@ -1,13 +1,28 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const path = require("path");
 
 module.exports = {
-  entry: ["./src/students.js"],
+  entry: {
+    app: "./src/students.js",
+    appStyles: "./src/mystyles.css",
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
@@ -16,10 +31,10 @@ module.exports = {
       template: "./src/index.html",
       filename: "index.html",
       scriptLoading: "blocking",
-      hash: true, 
     }),
+    new CleanWebpackPlugin(),
   ],
   devServer: {
     port: "8081",
-  }
+  },
 };
