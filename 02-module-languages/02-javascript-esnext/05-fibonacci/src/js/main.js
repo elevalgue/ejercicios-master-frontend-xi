7f98151ccdc8b1b70ef8185ff00f67efcb8ab088
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-02 21:15:05 +0100

    Solve fibonacci exercise

--- 02-module-languages/02-javascript-esnext/05-fibonacci/src/js/main.js
+++ 02-module-languages/02-javascript-esnext/05-fibonacci/src/js/main.js
@@ -1,34 +1,24 @@
 console.log('************** PRACTICE *********************');
 console.log('05 - Fibonacci');
 
-const fibonacci = [0, 1];
 
 const fib = (n) => {
+  let fibonacci = [0, 1];
   let [firstValue, secondValue] = fibonacci;
 
-  if (n === 0)  {
-    return firstValue
+  if (n === 0) {
+    return firstValue; 
   }
-
   if (n === 1) {
-    return secondValue
+    return secondValue;
   }
 
-  for (let index = 2; index < n; index++) {
+  for (let index = 2; index <= n; index++) {
     [firstValue, secondValue] = [secondValue, firstValue + secondValue];
   }
   return secondValue;
-  
 }
+console.log(fib(6));
 
-console.log(fib(7));
 
 
-// let a = 2;
-// console.log(a);
-// let b = 5;
-// console.log(b);
-
-// [a, b] = [b, a];
-// console.log(a, 'a');
-// console.log(b, 'b');
