04a7a17b8e793dc800b5fb867a93e007b5c60dad
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-12 19:57:20 +0100

    Add imports

---
+++ 02-module-languages/src/index.ts
@@ -0,0 +1,18 @@
+import "./01 deliverables/exercise-01.js";
+
+import "./01-javascript-fundamentals/01-biggest-word/main"; 
+import "./01-javascript-fundamentals/02-califications/main"; 
+import "./01-javascript-fundamentals/03-check-arguments"; 
+import "./01-javascript-fundamentals/04-clone-merge/main"; 
+import "./01-javascript-fundamentals/05-deepEqual/main"; 
+import "./01-javascript-fundamentals/06-dices/main"; 
+import "./01-javascript-fundamentals/07-hoisting/main"; 
+import "./01-javascript-fundamentals/08-includes/main"
+import "./01-javascript-fundamentals/09-primes";
+import "./01-javascript-fundamentals/10-read-book/main";
+import "./01-javascript-fundamentals/11-reverse-text";
+import "./01-javascript-fundamentals/12-subsets/main";
+import "./01-javascript-fundamentals/13-this/main";
+import "./01-javascript-fundamentals/14-values/main";
+import "./01-javascript-fundamentals/15-zip/main";
+import "./01-javascript-fundamentals/16-zzcrypt/main";
