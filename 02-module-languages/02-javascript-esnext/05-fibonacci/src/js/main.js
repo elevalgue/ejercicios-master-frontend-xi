8c8cad5122fdf5376b40378687bc8576ec131ca4
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-12 01:47:36 +0100

    Refactor :hammer:

--- 02-module-languages/02-javascript-esnext/05-fibonacci/src/js/main.js
+++ 02-module-languages/02-javascript-esnext/05-fibonacci/src/js/main.js
@@ -6,19 +6,15 @@ const fib = (n) => {
   let fibonacci = [0, 1];
   let [firstValue, secondValue] = fibonacci;
 
-  if (n === 0) {
-    return firstValue; 
-  }
-  if (n === 1) {
-    return secondValue;
-  }
+  if (n === 0) return firstValue; 
+  if (n === 1) return secondValue;
 
   for (let index = 2; index <= n; index++) {
     [firstValue, secondValue] = [secondValue, firstValue + secondValue];
   }
   return secondValue;
 }
-console.log(fib(6));
+console.log(fib(3));
 
 
 
