2d6a6e6bfc8451a55e1431b99a265aac3154d002
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-12 20:24:06 +0100

    Add new folder

--- 02-module-languages/src/index.ts
+++ 02-module-languages/src/index.ts
@@ -1,28 +1,9 @@
-// JS fundamentals
-import "./01-javascript-fundamentals/01-biggest-word/main"; 
-import "./01-javascript-fundamentals/02-califications/main"; 
-import "./01-javascript-fundamentals/03-check-arguments"; 
-import "./01-javascript-fundamentals/04-clone-merge/main"; 
-import "./01-javascript-fundamentals/05-deepEqual/main"; 
-import "./01-javascript-fundamentals/06-dices/main"; 
-import "./01-javascript-fundamentals/07-hoisting/main"; 
-import "./01-javascript-fundamentals/08-includes/main"
-import "./01-javascript-fundamentals/09-primes";
-import "./01-javascript-fundamentals/10-read-book/main";
-import "./01-javascript-fundamentals/11-reverse-text";
-import "./01-javascript-fundamentals/12-subsets/main";
-import "./01-javascript-fundamentals/13-this/main";
-import "./01-javascript-fundamentals/14-values/main";
-import "./01-javascript-fundamentals/15-zip/main";
-import "./01-javascript-fundamentals/16-zzcrypt/main";
+import "./00 practice/exercise.js";
+import "./01 deliverables/exercise-01.js";
+import "./01 deliverables/exercise-02.js";
+import "./01 deliverables/exercise-03.js";
+import "./01 deliverables/exercise-04.js";
+import "./01 deliverables/exercise-05.js";
+import "./02 challenges/exercise.js";
 
-// JS ESNext
-import "./02-javascript-esnext/01-args/main";
-import "./02-javascript-esnext/02-array-operations/main";
-import "./02-javascript-esnext/03-concat/main";
-import "./02-javascript-esnext/04-console/main";
-import "./02-javascript-esnext/05-fibonacci/main";
-import "./02-javascript-esnext/06-players-order/main";
-import "./02-javascript-esnext/07-reminder/main";
-import "./02-javascript-esnext/08-slot-machine/main";
-import "./02-javascript-esnext/09-swap/main";
+console.log("Welcome to Exercises Playground.");
