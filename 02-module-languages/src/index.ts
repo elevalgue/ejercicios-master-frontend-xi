91fa775901fb5b83a6b5348b2c14869c523c3544
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-13 10:07:28 +0100

    Fix bug :bug:

--- 02-module-languages/src/index.ts
+++ 02-module-languages/src/index.ts
@@ -1,19 +1,20 @@
 // 00 practice
-import "./00 practice/01-biggest-word";
-import "./00 practice/02-califications";
-import "./00 practice/03-check-arguments";
-import "./00 practice/04-clone-merge";
-import "./00 practice/05-deepEqual";
-import "./00 practice/06-dices";
-import "./00 practice/07-hoisting";
-import "./00 practice/08-includes";
-import "./00 practice/09-primes";
-import "./00 practice/10-read-book";
-import "./00 practice/11-reverse-text";
-import "./00 practice/12-subsets";
-import "./00 practice/13-this";
-import "./00 practice/14-values";
-import "./00 practice/15-zip";
-import "./00 practice/16-zzcrypt";
+// import "./00 practice/01-biggest-word.js";
+// import "./00 practice/02-califications.js";
+// import "./00 practice/03-check-arguments.js";
+// import "./00 practice/04-clone-merge.js";
+// import "./00 practice/05-deepEqual.js";
+// import "./00 practice/06-dices.js";
+// import "./00 practice/07-hoisting.js";
+// import "./00 practice/08-includes.js";
+// import "./00 practice/09-primes.js";
+// import "./00 practice/10-read-book.js";
+// import "./00 practice/11-reverse-text.js";
+// import "./00 practice/12-subsets.js";
+// import "./00 practice/13-this.js";
+// import "./00 practice/14-values.js";
+// import "./00 practice/15-zip.js";
+// import "./00 practice/16-zzcrypt.js";
 
 // 01 deriverables
+// import "./01 deliverables/exercise-01.js"; 
