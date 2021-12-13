a88a27320e61b7a441f31b34dd7998939751fc3c
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-13 11:14:35 +0100

    Install & config bundle analizer

--- 03-module-bundling/prueba/webpack.dev.js
+++ 03-module-bundling/prueba/webpack.dev.js
@@ -1,24 +1,22 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const path = require("path");
-const Dotenv = require("dotenv-webpack"); 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = merge(common, {
-  mode: "development",
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.scss$/,
         exclude: /node_modules/,
         use: [
-          "style-loader",
+          MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
             options: {
               modules: {
                 exportLocalsConvention: "camelCase",
-                localIdentName: "[path][name]__[local]--[hash:base64:5]",
-                localIdentContext: path.resolve(__dirname, "src/components"),
               },
             },
           },
@@ -27,14 +25,25 @@ module.exports = merge(common, {
       },
     ],
   },
-  devtool: "eval-source-map",
-  devServer: {
-    port: "8081",
-  },
-  stats: "errors-only",
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
+    }),
     new Dotenv({
-    path: "./dev.env", 
-  }),
+      path: "./prod.env",
+    }),
   ],
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: "all",
+          name: "vendor",
+          test: /[\\/]node_modules[\\/]/,
+          enforce: true,
+        },
+      },
+    },
+  },
 });
--- 03-module-bundling/prueba/webpack.prod.js
+++ 03-module-bundling/prueba/webpack.prod.js
@@ -3,7 +3,6 @@ const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const Dotenv = require("dotenv-webpack"); 
 
-
 module.exports = merge(common, {
   mode: "production",
   module: {
@@ -33,5 +32,17 @@ module.exports = merge(common, {
     new Dotenv({
     path: "./prod.env", 
   }),
-  ],
+  ],optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: "all",
+          name: "vendor",
+          test: /[\\/]node_modules[\\/]/,
+          enforce: true,
+        },
+      },
+    },
+  },
 });
