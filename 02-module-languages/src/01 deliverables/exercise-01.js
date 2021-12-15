1168bff28581ba0a7788c3796767ffb2b4226de7
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-15 19:17:08 +0100

    Add small changes

--- 02-module-languages/src/01 deliverables/exercise-01.js
+++ 02-module-languages/src/01 deliverables/exercise-01.js
@@ -1,33 +1,35 @@
 console.log("************** DELIVERABLE 01 *********************");
 
-console.log('/*----- 1. Head -----*/')
-
-const fruits = ["Apple", "Banana", "Watermelon"];
+console.log('/*------ 1. Head ------*/')
+const letters = ["a", "b", "c", "d", "e"]; 
 
 const head = (array) => {
-  const [first, ...rest] = fruits;
+  const [first, ...rest] = letters;
   return first; 
 };
 
-const head2 = ([first, ...rest]) => first; 
+const head2 = ([first, ...rest]) => first;
 
-console.log(head(fruits));
-console.log(head2(fruits));
+console.log(head(letters));
+console.log(head2(letters));
+console.log("/*---------------------*/");
 
-console.log('/*----- 2. Tail -----*/')
 
+console.log('/*------ 2. Tail ------*/')
 const tail = (array) => {
-  const [ , ...rest] = fruits;
+  const [ , ...rest] = letters;
   return rest; 
 }
 
 const tail2 = ([, ...rest]) => rest;
 
-console.log(tail(fruits));
-console.log(tail2(fruits));
+console.log(tail(letters));
+console.log(tail2(letters));
+console.log("/*---------------------*/");
 
-console.log('/*----- 3. Init -----*/')
 
-const init = (array) => fruits.slice(0, fruits.length - 1);
+console.log('/*------ 3. Init ------*/')
+const init = (array) => letters.slice(0, letters.length - 1);
 
-console.log(init(fruits));
+console.log(init(letters));
+console.log("/*---------------------*/");
