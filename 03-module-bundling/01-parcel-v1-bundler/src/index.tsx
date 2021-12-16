d17ccfdcfeadefc6bfbfa77fd9e0df2a0261bee9
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-17 00:15:39 +0100

    Change react entry point

---
+++ 03-module-bundling/01-parcel-v1-bundler/src/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Hello } from "hello";
+
+// import "./mystyles.scss";
+// import quokkaImg from "./content/quokka.jpg";
+
+// const userName = "Maricarmen";
+// const message = `Hello ${userName}`;
+
+// console.log(message);
+
+// const img = document.querySelector("img");
+// img.src = quokkaImg;
+
+// document.getElementById('imgContainer').appendChild(img); 
+
+ReactDOM.render(<Hello/>, document.getElementById("root")); 
