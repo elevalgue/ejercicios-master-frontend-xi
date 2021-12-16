986491090cd8333f268ea63c1253b304fd757482
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-17 00:43:01 +0100

    Clean :broom:

--- 03-module-bundling/01-parcel-v1-bundler/src/hello.tsx
+++ 03-module-bundling/01-parcel-v1-bundler/src/hello.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+const quokkaImg  = require("./content/quokka.jpg");
+const classes = require("./mystyles.scss");
 
 export const Hello: React.FC = () => {
-  return <h2>Hello from React</h2>
+  return (
+    <div className={classes.hello}>
+      <img src={quokkaImg} className={classes.quokkaImg} />
+      <h2>Hello from React</h2>
+    </div>
+  )
 }
--- 03-module-bundling/01-parcel-v1-bundler/src/index.tsx
+++ 03-module-bundling/01-parcel-v1-bundler/src/index.tsx
@@ -2,17 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Hello } from "./hello";
 
-// import "./mystyles.scss";
-// import quokkaImg from "./content/quokka.jpg";
-
-// const userName = "Maricarmen";
-// const message = `Hello ${userName}`;
-
-// console.log(message);
-
-// const img = document.querySelector("img");
-// img.src = quokkaImg;
-
-// document.getElementById('imgContainer').appendChild(img); 
-
-ReactDOM.render(<Hello/ >, document.getElementById("root")); 
+ReactDOM.render(<Hello />, document.getElementById("root"));
