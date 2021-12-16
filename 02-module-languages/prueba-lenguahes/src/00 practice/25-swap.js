2a6c968e16d2a490a72ac6ac491d53d1941b3253
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-16 15:36:16 +0100

    Remove folder :fire:

--- 02-module-languages/prueba-lenguahes/src/00 practice/25-swap.js
+++
@@ -1,8 +0,0 @@
-console.log('************** PRACTICE *********************');
-console.log('09 - Swap');
-
-let a = "A";
-let b = "B";
-
-[a, b] = [b, a]; 
-console.log(a === "B" && b === "A" ? "You did it!" : "Keep trying!");
--- 02-module-languages/prueba-lenguahes/src/index.ts
+++
@@ -1,5 +0,0 @@
-// 01 deriverables
-// import "./01 deliverables/exercise-01.js";
-// import "./01 deliverables/exercise-02.js";
-// import "./01 deliverables/exercise-03.js";
-import "./01 deliverables/exercise-04.ts";
