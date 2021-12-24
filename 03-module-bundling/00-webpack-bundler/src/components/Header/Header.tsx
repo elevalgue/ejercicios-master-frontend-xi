a4fdaf84dcee939dc596ccb9da05ebbaa3f05679
elena <elena.valenzuela.guerrero@gmail.com>
2021-12-24 03:14:30 +0100

    Add first approach to teachers hook

--- 03-module-bundling/00-webpack-bundler/src/components/Header/Header.tsx
+++ 03-module-bundling/00-webpack-bundler/src/components/Header/Header.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import classes from "../../styles/Header.scss"
+const teachersName = ["Antonio", "Victor", "Braulio"]
 
+// const helloTeachers = (props) => {
+  
+// }
 
-export const Header: React.FC = ()  => {
+export const Header: React.FC = (props)  => {
+  const [teachers, setTeachears] = useState(0);
+  
+  useEffect(() => {
+    setInterval(() => {
+      setTeachears((teachers) => (teachers === 2 ? 0 : teachers + 1));
+    }, 1500);
+  }, []);
 
   return (
     <header className={classes.header}>
         <h1 className={classes.header__title}>
-          Hello World! 👋
+        Hello, 
+        <span className={classes.skewed}>   {teachersName[teachers]}!</span>
         </h1>  
     </header>
   );
