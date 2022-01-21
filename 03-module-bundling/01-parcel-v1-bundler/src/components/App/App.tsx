8079f20e4ad2b8c48e1cd2a5d91cac12a7e518d9
elena <elena.valenzuela.guerrero@gmail.com>
2022-01-21 21:21:11 +0100

    Clean

--- 03-module-bundling/01-parcel-v1-bundler/src/components/App/App.tsx
+++ 03-module-bundling/01-parcel-v1-bundler/src/components/App/App.tsx
@@ -1,22 +1,15 @@
 import React from "react";
-import starCatcher from "../../content/star-catcher.jpg"
-import quokka from "../../content/quokka.jpg"
+import quokkaImg from "../../content/quokka.jpg"
 import classes from "../../styles/App.scss";
-// const classes = require("../../styles/App.scss"); 
 const classes = require("../../styles/App.scss"); 
-
-
-// import classes = require ("../../styles/App.scss"); 
-
 
 export const App: React.FC = () => {
 
   return (
       <div className={classes.container}>
-        <img className={classes.container__image} src={quokka}/>
+        <img className={classes.container__image} src={quokkaImg}/>
       </div>
   );
 };
 
 
-      // <img src={quokkaImg} className={classes.quokkaImg} />
--- 03-module-bundling/01-parcel-v1-bundler/src/index.tsx
+++ 03-module-bundling/01-parcel-v1-bundler/src/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { Hello } from "./hello";
 
-import { App } from "./components/App/App";
 import { HelloWorld } from "components/HelloWorld/HelloWorld";
-
-// ReactDOM.render(<Hello />, document.getElementById("root"));
+import { App } from "./components/App/App";
 
 ReactDOM.render(
   <div>
