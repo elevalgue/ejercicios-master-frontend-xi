2354f7aa4c1adf1e98584f43c325e37c44eeccbe
elena <elena.valenzuela.guerrero@gmail.com>
2022-03-04 20:23:48 +0100

    Add member pod folder

--- 04-module-frameworks/01-react/00-basic/src/pods/member-list/components/member-row.component.tsx
+++ 04-module-frameworks/01-react/00-basic/src/pods/member-list/components/member-row.component.tsx
@@ -7,7 +7,21 @@ interface Props {
 
 export const MemberRow: React.FC<Props> = props => {
 
-  return <></>
+  return <>
+          <tr>
+              <td>
+                <img src={member.avatar_url} style={{ width: "5rem" }} />
+              </td>
+              <td>
+                <span>{member.id}</span>
+              </td>
+              <td>
+                <Link to={generatePath("/detail/:id", { id: member.login })}>
+                  {member.login}
+                </Link>
+              </td>
+            </tr>
+  </>
 }
 
 
